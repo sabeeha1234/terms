678fa9ec21b08182adda1f753c1851cc118247a3
sabeeha1234 <142984283+sabeeha1234@users.noreply.github.com>
2024-02-16 10:30:17 +0000

    fff

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+//use strict;
+let user="sabeeha"
+let age=14
+let isLoggedIn=true
+const number=12345
+let number3=BigInt(21712837219290293278179382908913872191098290810892382737377373737373737377)
+console.log(typeof user)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof number3)
+
---
+++ basics/test.js
@@ -0,0 +1,10 @@
+const userId=12345
+let userName="sabeeha"
+let userEmail="s.sabeeha8784@gmail.com"
+let state="andhrapradesh"
+console.log(userId);
+console.log(userId,userName,state,userEmail);
+console.table([userId,userName,state,userEmail]);
+userName="shaiksabeeha"
+console.log(userName)
+
