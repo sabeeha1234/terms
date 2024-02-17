f8b652390e4a222726f6e5c64aec8b87b2f50b3b
sabeeha1234 <142984283+sabeeha1234@users.noreply.github.com>
2024-02-17 10:58:21 +0000

    maa

---
+++ basics/comparison.js
@@ -0,0 +1,25 @@
+// let x=[]
+// let y=[]
+// console.log(x===y);
+// const me="shaheed";
+// const you="sabeeha";
+
+// console.log(`hello my name is ${me}and my friends nameis ${you}`);
+
+const name="   shaiksabe  ehahahaha"
+console.log(name[0]);
+console.log(name.length);
+console.log(name.slice(2,8));
+console.log(name.indexOf("b"));
+console.log(name.lastIndexOf("b"));
+console.log(name.toUpperCase());
+console.log(name.trim());
+
+
+console.log(name.replace('shaiksabeehahahaha','hvdyhghgw dqhshdffskUWGVHBVMHFGFG'));
+console.log(name.includes('hvdyhghgw'));
+console.log(name.split('-'));
+
+const namwe="sabbu  uuuoooo"
+const word=name.split(' ')
+console.log(word[6]);
---
+++ basics/operations.js
@@ -0,0 +1,10 @@
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3+4)*5%3);
+console.log(+true);
+num1=num2=num3=5
+console.log(num1+num2);
+let value=300
+++value
+console.log(value);
